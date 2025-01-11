a96eaa138fd3a87f0378d37fa506724e855c97fb
Iliasnolsson <58471856+Iliasnolsson@users.noreply.github.com>
2025-01-11 16:08:40 +0100

    - Fix to support vite.config.js

--- src/middleware/find-components/findViteConfig.ts
+++ src/middleware/find-components/findViteConfig.ts
@@ -1,14 +1,16 @@
 import * as path from "path";
-import fs from "fs"
+import fs from "fs";
 
 export default function findViteConfig(currentDir: string): string | null {
-    const configFileName = 'vite.config.ts';
+    const configFileNames = ['vite.config.ts', 'vite.config.js']; // Support both .ts and .js
     let directory = currentDir;
 
     while (true) {
-        const configPath = path.join(directory, configFileName);
-        if (fs.existsSync(configPath)) {
-            return configPath;
+        for (const configFileName of configFileNames) {
+            const configPath = path.join(directory, configFileName);
+            if (fs.existsSync(configPath)) {
+                return configPath;
+            }
         }
 
         const parentDirectory = path.dirname(directory);
